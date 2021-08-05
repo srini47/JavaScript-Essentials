87135c2a453b310f7faa20f1d040d3a0e14bd77d
srini47 <86734233+srini47@users.noreply.github.com>
2021-08-05 13:38:52 +0530

    Add files via upload

---
+++ Assignment 2/Assignment2.js
@@ -0,0 +1,16 @@
+let names = ["Letsupgarde", "Me", "You", "Him", "Her"]
+console.log(names);
+for (i=names.length; i>=0; i--)
+{
+    console.log(names[i]);
+}
+
+console.log("")
+
+for (x=1; x<=50; x++)
+{
+    if (x%5 == 0)
+    {
+        console.log(x);
+    }
+}
